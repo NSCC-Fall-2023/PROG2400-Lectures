966e0fd42dd692c57f2984a7b5ba00a9c5e71c2c
Brian Shewan <brian.shewan@nscc.ca>
2023-10-04 21:59:39 -0300

    modified stack to use students

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <utility>
+
+struct Student {
+    std::string m_id   {"W999999"};
+    std::string m_name {"No Name"};
+};
 
 struct Node {
-    int m_data {-1};
+    Student m_data;
     Node* m_next {nullptr};
 };
 
@@ -10,9 +16,9 @@ private:
     Node* m_top {nullptr};
 
 public:
-    void push(int data) {
+    void push(Student data) {
         auto node = new Node();
-        node->m_data = data;
+        node->m_data = std::move(data);
         node->m_next = m_top;
         m_top = node;
     }
@@ -23,17 +29,28 @@ public:
         delete node;
     }
 
-    int peek() {
-        return m_top ? m_top->m_data : -1;
+    Student peek() {
+        if (m_top == nullptr) return {"W000000", "Empty Peek"};
+        return m_top->m_data;
+    }
+
+    bool empty() {
+        return m_top == nullptr;
     }
 
     friend std::ostream& operator<<(std::ostream& output, Stack& stack);
 };
 
+std::ostream& operator<<(std::ostream& output, const Student& student) {
+    output << "Name: " << student.m_name << ", ";
+    output << "ID: " << student.m_id << std::endl;
+    return output;
+}
+
 std::ostream& operator<<(std::ostream& output, Stack& stack) {
     auto node = stack.m_top;
     while (node != nullptr) {
-        std::cout << node->m_data << ", ";
+        std::cout << node->m_data;
         node = node->m_next;
     }
 
@@ -45,11 +62,11 @@ int main() {
     Stack stack;
 
     // test 1 - add some nodes to the stack
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    stack.push(4);
-    stack.push(5);
+    stack.push({"W111222", "John"});
+    stack.push({"W222333", "Jane"});
+    stack.push({"W333444", "Jill"});
+    stack.push({"W444555", "Jack"});
+    stack.push({"W555666", "Jeff"});
 
     std::cout << "Test 1 - adding nodes to stack" << std::endl;
     std::cout << "------------------------------" << std::endl;
@@ -70,7 +87,7 @@ int main() {
     // test 4 - remove all nodes from stack
     std::cout << "Test 4 - remove all data from stack" << std::endl;
     std::cout << "-----------------------------------" << std::endl;
-    while (stack.peek() != -1) {
+    while (!stack.empty()) {
         std::cout << stack << std::endl;
         stack.pop();
     }
